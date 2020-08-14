f41e4c6b12c181e6d28b5803417260cf6467ecba
swagking0 <bhargav.mohith@gmail.com>
2020-08-15 01:54:15 +0200

    feature/loader

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Loader from "./resources/base/components/sub-components/Loader";
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [myData, setmyData] = useState(null);
+
+  useEffect(() => {
+    const getmyData = () => {
+      return fetch("https://gitconnected.com/v1/portfolio/swagking0", {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((myData) => {
+          setmyData(myData);
+        });
+    };
+    getmyData().then(() => {
+      setLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Website Under Development</h1>
-        <p>Upgrading to React!</p>
+        <p>Upgrading to React!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         ></a>
+        <h2>{myData.basics.name}</h2>
       </header>
     </div>
   );
---
+++ src/resources/base/components/sub-components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../styles/subcomp-styles/loader.css";
+
+function Loader() {
+  return (
+    <div className="loader-container">
+      <h1>Welcome</h1>
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
